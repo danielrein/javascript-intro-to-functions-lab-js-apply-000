a462b4589cbc470359865127fec9d6d679d3c571
danielrein <danielr1735@gmail.com>
2018-06-07 04:09:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
   return console.log(string.toLowerCase())
 }
 
-// function sayHiToGrandma(string) {
-//   if string.toLowerCase() === string { return 'I can\'t hear you!' }
-//   else if string.toUpperCase() === string { return 'YES INDEED!' }
-//   else if string === 'I love you, Grandma.' { return 'I love you, too.'}
-// }
+function sayHiToGrandma(string) {
+  if string.toLowerCase() === string { return 'I can\'t hear you!' }
+  else if string.toUpperCase() === string { return 'YES INDEED!' }
+  else if string === 'I love you, Grandma.' { return 'I love you, too.'}
+}
